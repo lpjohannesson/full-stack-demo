42f3ca436bbc2aa7427ec443d0059e318cf1de10
Leif Johannesson <lp.johannesson@gmail.com>
2025-05-30 01:30:20 -0400

    Indent fix

--- frontend/src/UserContext.tsx
+++ frontend/src/UserContext.tsx
@@ -2,8 +2,8 @@ import { createContext, type Dispatch, type SetStateAction } from 'react'
 import type { UserModel } from './api/models/UserModel';
 
 interface UserContextType {
-  user: UserModel | null;
-  setUser: Dispatch<SetStateAction<UserModel | null>>;
+    user: UserModel | null;
+    setUser: Dispatch<SetStateAction<UserModel | null>>;
 }
 
 export const UserContext = createContext<UserContextType>({
